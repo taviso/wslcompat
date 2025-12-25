75e1fb26259b9d563f6f9f334780b4f9c18f1ea8
Tavis Ormandy <taviso@gmail.com>
2025-12-25 10:43:30 -0800

    test new approach

--- mmap.c
+++ mmap.c
@@ -9,46 +9,46 @@
 #include <stdio.h>
 
 static void * (*sym_mmap)(void *, size_t, int, int, int, off_t);
+static void * (*sym_mmap64)(void *, size_t, int, int, int, off64_t);
+
+static FILE *maps;
 
 static void __attribute__((constructor)) init(void)
 {
     sym_mmap = dlsym(RTLD_NEXT, "mmap");
+    sym_mmap64 = dlsym(RTLD_NEXT, "mmap64");
     return;
 }
 
-static bool check_page_mapped(uintptr_t ptr) {
-    FILE *f;
+static void __attribute__((destructor)) fini(void)
+{
+    if (maps) fclose(maps);
+    return;
+}
+
+static bool check_range_mapped(uintptr_t ptr, size_t len) {
     uintptr_t start;
     uintptr_t end;
-    bool result = false;
+    bool result = true;
 
-    f = fopen("/proc/self/maps", "r");
+    if (maps == NULL) {
+        maps = fopen("/proc/self/maps", "r");
+    }
 
-    while (fscanf(f, "%lx-%lx %*s %*x %*x:%*x %*d %*s\n", &start, &end) == 2) {
-        if (ptr >= start && ptr < end) {
-            result = true;
+    rewind(maps);
+
+    while (fscanf(maps, "%lx-%lx %*s %*x %*x:%*x %*d %*s\n", &start, &end) == 2) {
+        if (start >= ptr && ptr + len < end) {
+            result = false;
             break;
         }
     }
 
-    fclose(f);
     return result;
-}
-
-static bool check_range_mapped(uintptr_t ptr, size_t length) {
-    size_t pagecnt = (length + PAGE_SIZE - 1) / PAGE_SIZE;
-
-    for (size_t i = 0; i < pagecnt; i++) {
-        if (check_page_mapped(ptr + i * PAGE_SIZE))
-            return true;
-    }
-    return false;
 }
 
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
-    uint8_t vec[1024] = {0};
-
     // Handle any flags that WSL1 rejects
     if (flags & MAP_FIXED_NOREPLACE) {
         // So we can emulate this by returning EEXIST if the mapping already
@@ -66,4 +66,25 @@ void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
     }
 
     return sym_mmap(addr, length, prot, flags, fd, offset);
+}
+
+void *mmap64(void *addr, size_t length, int prot, int flags, int fd, off64_t offset)
+{
+    // Handle any flags that WSL1 rejects
+    if (flags & MAP_FIXED_NOREPLACE) {
+        // So we can emulate this by returning EEXIST if the mapping already
+        // exists.
+        flags &= ~MAP_FIXED_NOREPLACE;
+        flags |=  MAP_FIXED;
+
+        if (check_range_mapped((uintptr_t)(addr), length)) {
+            // This should fail with EEXIST
+            errno = EEXIST;
+
+            // Return failure
+            return MAP_FAILED;
+        }
+    }
+
+    return sym_mmap64(addr, length, prot, flags, fd, offset);
 }
