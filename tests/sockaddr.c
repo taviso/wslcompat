97bacbc0af02f387bdfb26c32e3b8b2b23a98cf0
Tavis Ormandy <taviso@gmail.com>
2026-05-02 16:20:29 -0700

    add sockaddr to automated tests

--- tests/sockaddr.c
+++ tests/sockaddr.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <err.h>
 
 int main(int argc, char **argv) {
     int fd;
@@ -23,7 +24,7 @@ int main(int argc, char **argv) {
             printf("socket is AF_UNIX\n");
         }
     } else {
-        perror("getsockname");
+        err(EXIT_FAILURE, "getsockname failed");
     }
 
     // Get AF
@@ -33,13 +34,13 @@ int main(int argc, char **argv) {
             printf("socket is AF_UNIX\n");
         }
     } else {
-        perror("getsockopt");
+        err(EXIT_FAILURE, "getsockopt SOL_SOCKET failed");
     }
 
     // Get protocol
     len = sizeof(protocol);
     if (getsockopt(fd, SOL_SOCKET, SO_PROTOCOL, &protocol, &len) == -1) {
-        perror("getsockopt");
+        err(EXIT_FAILURE, "getsockopt SO_PROTOCOL failed");
     } else {
         printf("SO_PROTOCOL for AF_UNIX socket: %d\n", protocol);
     }
@@ -47,26 +48,28 @@ int main(int argc, char **argv) {
     // Get type
     len = sizeof(type);
     if (getsockopt(fd, SOL_SOCKET, SO_TYPE, &type, &len) == -1) {
-        perror("getsockopt");
+        err(EXIT_FAILURE, "getsockopt SO_TYPE failed");
     } else {
         printf("SO_TYPE for AF_UNIX socket: %d\n", protocol);
     }
 
+#if 0
     // Check SO_TIMESTAMP
     int timestamp;
     len = sizeof(timestamp);
     if (getsockopt(fd, SOL_SOCKET, SO_TIMESTAMP, &timestamp, &len) == -1) {
-        perror("getsockopt SO_TIMESTAMP");
+        err(EXIT_FAILURE, "getsockopt SO_TIMESTAMP failed");
     } else {
         printf("SO_TIMESTAMP is %d\n", timestamp);
     }
 
     timestamp = 1;
     if (setsockopt(fd, SOL_SOCKET, SO_TIMESTAMP, &timestamp, sizeof(timestamp)) == -1) {
-        perror("setsockopt SO_TIMESTAMP");
+        err(EXIT_FAILURE, "setsockopt SO_TIMESTAMP failed");
     } else {
         printf("Successfully set SO_TIMESTAMP to 1\n");
     }
+#endif
 
     close(fd);
     return 0;
