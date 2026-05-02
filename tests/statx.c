44173dc31e33dc5589895389d13afaf4c40ed195
Tavis Ormandy <taviso@gmail.com>
2026-05-02 16:00:27 -0700

    make an effort to cleanup tests

--- tests/statx.c
+++ tests/statx.c
@@ -14,81 +14,92 @@
 # define STATX_ATTR_MOUNT_ROOT 0x2000
 #endif
 
-void check_stx_mask(struct statx *stx, const char *name, int flag)
+int check_stx_mask(struct statx *stx, const char *name, int flag)
 {
-    if (stx->stx_mask & flag)
+    if (stx->stx_mask & flag) {
         printf("stx_mask *did* provide %s\n", name);
-    else
-        printf("stx_mask *did not* provide %s\n", name);
-    return;
+        return 1;
+    }
+    printf("stx_mask *did not* provide %s\n", name);
+    return 0;
 }
 
-void check_stx_attribute(struct statx *stx, const char *name, uint64_t flag)
+int check_stx_attribute(struct statx *stx, const char *name, uint64_t flag)
 {
     if (stx->stx_attributes_mask & flag) {
-        if (stx->stx_attributes & flag)
+        if (stx->stx_attributes & flag) {
             printf("stx_attributes *did* have %s set\n", name);
-        else
-            printf("stx_attributes *did not* have %s set\n", name);
-    } else {
-        printf("stx_attributes_mask *did not* include %s\n", name);
+            return 1;
+        }
+        printf("stx_attributes *did not* have %s set\n", name);
+        return 0;
     }
+
+    printf("stx_attributes_mask *did not* include %s\n", name);
+    return 0;
 }
 
 int main() {
     struct statx stx = {0};
-    int ret;
 
     printf("--- Testing /etc/passwd ---\n");
-    ret = statx(AT_FDCWD, "/etc/passwd", 0, STATX_BASIC_STATS | STATX_BTIME | STATX_MNT_ID, &stx);
 
-    if (ret == -1) {
-        err(EXIT_FAILURE, "statx failed");
+    if (statx(AT_FDCWD, "/etc/passwd", 0, STATX_BASIC_STATS | STATX_MNT_ID, &stx) != 0) {
+        errx(EXIT_FAILURE, "statx failed");
     }
 
-    printf("statx: success\n");
+    //check_stx_mask(&stx, "STATX_TYPE", STATX_TYPE);
+    //check_stx_mask(&stx, "STATX_MODE", STATX_MODE);
+    //check_stx_mask(&stx, "STATX_NLINK", STATX_NLINK);
+    //check_stx_mask(&stx, "STATX_UID", STATX_UID);
+    //check_stx_mask(&stx, "STATX_GID", STATX_GID);
+    //check_stx_mask(&stx, "STATX_ATIME", STATX_ATIME);
+    //check_stx_mask(&stx, "STATX_MTIME", STATX_MTIME);
+    //check_stx_mask(&stx, "STATX_CTIME", STATX_CTIME);
+    //check_stx_mask(&stx, "STATX_INO", STATX_INO);
+    //check_stx_mask(&stx, "STATX_SIZE", STATX_SIZE);
+    //check_stx_mask(&stx, "STATX_BLOCKS", STATX_BLOCKS);
+    //check_stx_mask(&stx, "STATX_BTIME", STATX_BTIME);
 
-    check_stx_mask(&stx, "STATX_TYPE", STATX_TYPE);
-    check_stx_mask(&stx, "STATX_MODE", STATX_MODE);
-    check_stx_mask(&stx, "STATX_NLINK", STATX_NLINK);
-    check_stx_mask(&stx, "STATX_UID", STATX_UID);
-    check_stx_mask(&stx, "STATX_GID", STATX_GID);
-    check_stx_mask(&stx, "STATX_ATIME", STATX_ATIME);
-    check_stx_mask(&stx, "STATX_MTIME", STATX_MTIME);
-    check_stx_mask(&stx, "STATX_CTIME", STATX_CTIME);
-    check_stx_mask(&stx, "STATX_INO", STATX_INO);
-    check_stx_mask(&stx, "STATX_SIZE", STATX_SIZE);
-    check_stx_mask(&stx, "STATX_BLOCKS", STATX_BLOCKS);
-    check_stx_mask(&stx, "STATX_BTIME", STATX_BTIME);
-    check_stx_mask(&stx, "STATX_MNT_ID", STATX_MNT_ID);
     check_stx_attribute(&stx, "STATX_ATTR_MOUNT_ROOT", STATX_ATTR_MOUNT_ROOT);
 
-    printf("\n--- Testing / ---\n");
-    memset(&stx, 0, sizeof(stx));
-    ret = statx(AT_FDCWD, "/", 0, STATX_BASIC_STATS | STATX_MNT_ID, &stx);
-    if (ret == -1) {
-        err(EXIT_FAILURE, "statx failed on /");
+    if (check_stx_mask(&stx, "STATX_MNT_ID", STATX_MNT_ID) == false) {
+        err(EXIT_FAILURE, "requested mnt id but was not provided");
     }
-    printf("statx: success\n");
-    check_stx_attribute(&stx, "STATX_ATTR_MOUNT_ROOT", STATX_ATTR_MOUNT_ROOT);
 
-    printf("\n--- Testing /mnt/c ---\n");
-    memset(&stx, 0, sizeof(stx));
-    ret = statx(AT_FDCWD, "/mnt/c", 0, STATX_BASIC_STATS | STATX_MNT_ID, &stx);
-    if (ret == -1) {
-        err(EXIT_FAILURE, "statx failed on /mnt/c");
+    if (check_stx_attribute(&stx, "STATX_ATTR_MOUNT_ROOT", STATX_ATTR_MOUNT_ROOT) == true) {
+        errx(EXIT_FAILURE, "did not expect /etc/passwd to be amount point");
     }
-    printf("statx: success\n");
-    check_stx_attribute(&stx, "STATX_ATTR_MOUNT_ROOT", STATX_ATTR_MOUNT_ROOT);
 
-    printf("\n--- Testing /etc ---\n");
-    memset(&stx, 0, sizeof(stx));
-    ret = statx(AT_FDCWD, "/etc", 0, STATX_BASIC_STATS | STATX_MNT_ID, &stx);
-    if (ret == -1) {
-        err(EXIT_FAILURE, "statx failed on /etc");
+    printf("--- Testing / ---\n");
+
+    if (statx(AT_FDCWD, "/", 0, STATX_MNT_ID, &stx) != 0) {
+        errx(EXIT_FAILURE, "statx failed");
     }
-    printf("statx: success\n");
-    check_stx_attribute(&stx, "STATX_ATTR_MOUNT_ROOT", STATX_ATTR_MOUNT_ROOT);
+
+    if (check_stx_attribute(&stx, "STATX_ATTR_MOUNT_ROOT", STATX_ATTR_MOUNT_ROOT) == false) {
+        errx(EXIT_FAILURE, "expected / to be a mount point");
+    }
+
+    printf("--- Testing /mnt/c ---\n");
+
+    if (statx(AT_FDCWD, "/mnt/c", 0, STATX_BASIC_STATS | STATX_MNT_ID, &stx) != 0) {
+        errx(EXIT_FAILURE, "statx failed");
+    }
+
+    if (check_stx_attribute(&stx, "STATX_ATTR_MOUNT_ROOT", STATX_ATTR_MOUNT_ROOT) == false) {
+        errx(EXIT_FAILURE, "expected /mnt/c to be a mount point");
+    }
+
+    printf("--- Testing /etc ---\n");
+
+    if (statx(AT_FDCWD, "/etc", 0, STATX_MNT_ID, &stx) != 0) {
+        errx(EXIT_FAILURE, "statx failed");
+    }
+
+    if (check_stx_attribute(&stx, "STATX_ATTR_MOUNT_ROOT", STATX_ATTR_MOUNT_ROOT) == true) {
+        errx(EXIT_FAILURE, "did not expect /etc to be a mount point");
+    }
 
     return 0;
 }
