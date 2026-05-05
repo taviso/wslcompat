a0bdaa7c9046ce24e80c6456873748ad9ba67550
Tavis Ormandy <taviso@gmail.com>
2026-05-04 18:14:30 -0700

    we never call mincore, the lookup can be avoided

--- mincore.c
+++ mincore.c
@@ -1,17 +1,9 @@
 #define _GNU_SOURCE
 #include <sys/mman.h>
 #include <sys/user.h>
-#include <dlfcn.h>
 #include <errno.h>
 #include <unistd.h>
 #include <string.h>
-
-static int (*sym_mincore)(void *, size_t, unsigned char *);
-
-static void __attribute__((constructor)) init(void)
-{
-    sym_mincore = dlsym(RTLD_NEXT, "mincore");
-}
 
 int mincore(void *addr, size_t length, unsigned char *vec)
 {
