fbcdaabb76edd9fcaa7c59262f8a75250311cfcc
Tavis Ormandy <taviso@gmail.com>
2026-05-18 16:49:44 -0700

    handle automated runs

--- tests/tcgets.c
+++ tests/tcgets.c
@@ -5,11 +5,17 @@
 #include <string.h>
 #include <stdarg.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <asm/termbits.h>
 #include <sys/ioctl.h>
 
 int main(int argc, char **argv)
 {
     struct termios2 term;
+
+    // Stdin is not a tty (CI, piped shell, etc.); nothing to test.
+    if (!isatty(0))
+        return 0;
+
     return ioctl(0, TCGETS2, &term) != 0;
 }
