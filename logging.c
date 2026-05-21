b3d0bfe11e0b0813630da3d89af375c783cd3270
Tavis Ormandy <taviso@gmail.com>
2026-05-20 21:14:41 -0700

    add support for logging functions

---
+++ logging.c
@@ -0,0 +1,67 @@
+#define _GNU_SOURCE
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/uio.h>
+
+#include "tunables.h"
+#include "logging.h"
+
+static int loglevel = -1;
+static int debugfd  = -2;
+
+static void __attribute__((destructor)) fini(void)
+{
+    if (debugfd >= 0)
+        close(debugfd);
+}
+
+int wslcompat_debug_log(int level, const char *tag, const char *fmt, ...)
+{
+    char prefix[64];
+    char buf[512];
+    va_list ap;
+    struct iovec logbuf[] = {
+        { .iov_len = 0, .iov_base = prefix    },
+        { .iov_len = 0, .iov_base = buf       },
+        { .iov_len = 1, .iov_base = "\n"      },
+    };
+
+    // Check if logging is enabled.
+    if (loglevel < 0)
+        loglevel = wslcompat_tunable_int("debug", 0);
+
+    // Check if this log message is wanted.
+    if (level < loglevel)
+        return 0;
+
+    // Check if we have an output descriptor.
+    if (debugfd < -1)
+        debugfd = open("/dev/tty", O_WRONLY | O_CLOEXEC);
+
+    // If that files, nothing we can do.
+    if (debugfd < 0)
+        return -1;
+
+    // Optionall prepend a prefix string.
+    if (tag) {
+        logbuf[0].iov_len = snprintf(prefix, sizeof(prefix), "[wsl] %s: ", tag);
+    } else {
+        logbuf[0].iov_len = snprintf(prefix, sizeof(prefix), "[wsl] ");
+    }
+
+    // Prepare log message.
+    va_start(ap, fmt);
+    logbuf[1].iov_len = vsnprintf(buf, sizeof(buf), fmt, ap);
+    va_end(ap);
+
+    // Verify that message printed.
+    if (logbuf[0].iov_len < 0 || logbuf[1].iov_len <= 0) {
+        return -1;
+    }
+
+    // Pass to writev() to assumeble.
+    return writev(debugfd, logbuf, 3);
+}
---
+++ logging.h
@@ -0,0 +1,19 @@
+#ifndef WSLCOMPAT_LOGGING_H
+#define WSLCOMPAT_LOGGING_H
+
+enum {
+    WSLLOG_ERROR,
+    WSLLOG_WARNING,
+    WSLLOG_INFO,
+    WSLLOG_DEBUG,
+};
+
+__attribute__((format(printf, 3, 4)))
+int wslcompat_debug_log(int level, const char *tag, const char *fmt, ...);
+
+#define wsldbg(fmt, ...) wslcompat_debug_log(WSLLOG_DEBUG, __func__, fmt, ## __VA_ARGS__)
+#define wsllog(fmt, ...) wslcompat_debug_log(WSLLOG_INFO, __func__, fmt, ## __VA_ARGS__)
+#define wslwrn(fmt, ...) wslcompat_debug_log(WSLLOG_WARNING, __func__, fmt, ##__VA_ARGS__)
+#define wslerr(fmt, ...) wslcompat_debug_log(WSLLOG_ERROR, __func__, fmt, ## __VA_ARGS__)
+
+#endif
