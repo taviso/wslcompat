6aefbcd6297b9e926e785c0f385cf48400fe77fc
Tavis Ormandy <taviso@gmail.com>
2026-05-21 11:51:09 -0700

    improve execveat AT_PATH_EMPTY handling

--- execveat.c
+++ execveat.c
@@ -17,18 +17,21 @@ int execveat(int dirfd, const char *pathname,
     char        buf[PATH_MAX];
     const char *target;
 
+    // Optimization barrier: hide 'pathname' from GCC's nonnull analysis
+    asm volatile ("" : "+r" (pathname));
+
     if (wslcompat_passthru("execveat"))
         return sym_next(execveat, dirfd, pathname, argv, envp, flags);
 
-    if (pathname == NULL) {
-        errno = EFAULT;
-        return -1;
-    }
-
-    if (pathname[0] == '\0' && (flags & AT_EMPTY_PATH)) {
+    if ((!pathname || !*pathname) && (flags & AT_EMPTY_PATH)) {
         // fexecve-style: dirfd is the binary itself.
         snprintf(buf, sizeof(buf), "/proc/self/fd/%d", dirfd);
-    } else if (pathname[0] == '/' || dirfd == AT_FDCWD) {
+    } else if (pathname == NULL) {
+        errno = EFAULT;
+        return -1;
+    } else if (pathname[0] == '/') {
+        snprintf(buf, sizeof(buf), "%s", pathname);
+    } else if (dirfd == AT_FDCWD) {
         snprintf(buf, sizeof(buf), "%s", pathname);
     } else {
         snprintf(buf, sizeof(buf), "/proc/self/fd/%d/%s", dirfd, pathname);
