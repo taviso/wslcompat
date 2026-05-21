1ee1772776d67e119d626c760c7cd7f54b9cc6b0
Tavis Ormandy <taviso@gmail.com>
2026-05-21 08:24:48 -0700

    cleanup /tmp

--- tests/ofd_block.c
+++ tests/ofd_block.c
@@ -17,7 +17,10 @@ int main() {
 
     printf("--- Test: F_OFD_SETLKW blocking behavior ---\n");
     printf("Parent: Acquiring lock...\n");
-    if (fcntl(fd, F_OFD_SETLK, &fl) == -1) err(EXIT_FAILURE, "parent fcntl");
+    if (fcntl(fd, F_OFD_SETLK, &fl) == -1) {
+        unlink(tmpfile);
+        err(EXIT_FAILURE, "parent fcntl");
+    }
 
     pid_t pid = fork();
     if (pid == -1) err(EXIT_FAILURE, "fork");
@@ -29,7 +32,7 @@ int main() {
 
         printf("Child: Attempting F_OFD_SETLKW (should block)...\n");
         time_t start = time(NULL);
-        
+
         if (fcntl(fd2, F_OFD_SETLKW, &fl) == -1) {
             perror("Child: fcntl F_OFD_SETLKW failed");
             exit(1);
@@ -37,7 +40,7 @@ int main() {
 
         time_t end = time(NULL);
         printf("Child: Successfully acquired lock after %ld seconds.\n", (long)(end - start));
-        
+
         if ((end - start) < 2) {
             fprintf(stderr, "Child: Failure: Did not block for long enough!\n");
             exit(1);
@@ -50,7 +53,10 @@ int main() {
 
     printf("Parent: Releasing lock...\n");
     fl.l_type = F_UNLCK;
-    if (fcntl(fd, F_OFD_SETLK, &fl) == -1) err(EXIT_FAILURE, "parent release");
+    if (fcntl(fd, F_OFD_SETLK, &fl) == -1) {
+        unlink(tmpfile);
+        err(EXIT_FAILURE, "parent release");
+    }
 
     int status;
     waitpid(pid, &status, 0);
